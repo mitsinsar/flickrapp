22e63074553d2d47763c7f25e3a42211b587b5e6
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2020-04-03 20:54:18 +0300

    :sparkles: This is my commit message

--- app/src/main/java/com/kangarootech/flickr/datalayer/network/response/PhotoDetailsResponseDTO.kt
+++ app/src/main/java/com/kangarootech/flickr/datalayer/network/response/PhotoDetailsResponseDTO.kt
@@ -12,13 +12,11 @@ import com.kangarootech.flickr.datalayer.network.dto.photodetail.PhotoDetailDTO
 //           ʕ•ᴥ•ʔ         //
 ////////////////////////////
 
-
 data class PhotoDetailsResponseDTO(
     val photo: PhotoDetailDTO,
     val stat: String
 ) {
-    fun getOwnerIconUrl() =
-        "http://farm${photo.owner.iconFarm}.staticflickr.com/${photo.owner.iconServer}/buddyicons/${photo.owner.nsid}.jpg"
+    fun getOwnerIconUrl() = "http://farm${photo.owner.iconFarm}.staticflickr.com/${photo.owner.iconServer}/buddyicons/${photo.owner.nsid}.jpg"
 
     fun getImageUrl() = "https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg"
 }
